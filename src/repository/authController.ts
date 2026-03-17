4252647bf5bd87f52a62d6a86d3926ab35b27593
Tiago Santos <xlzthyago@gmail.com>
2026-03-17 10:59:35 -0300

    chore: implement type in connect database

--- src/repository/authController.ts
+++ src/repository/authController.ts
@@ -1,14 +1,22 @@
+
+import type { RowDataPacket } from 'mysql2/promise';
 import { connection } from '../database/connection.ts';
+
+interface UserRow extends RowDataPacket {
+  id: number;
+  email: string;
+  password: string;
+}
 
 export default class AuthRepository {
   public getByEmail = async (email: string) => {
     try {
-      const [rows] = await connection.query(
+      const [rows] = await connection.query<UserRow[]>(
         'SELECT * FROM users WHERE email = ?',
         [email]
       );
 
-      return rows
+      return rows[0];
     } catch (err) {
       console.error(`erro ao pegar email do usúaruio ${err}`);
     }
